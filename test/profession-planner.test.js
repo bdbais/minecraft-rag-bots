00be09d1a45dfcbff5b85d805db1203e63666fe2
bdbais <bais@bais.info>
2026-08-13 11:06:12 +0200

    Make warriors patrol protected bases

--- test/profession-planner.test.js
+++ test/profession-planner.test.js
@@ -45,6 +45,11 @@ test('warrior profession crafts a weapon before patrol', () => {
   assert.equal(decision.action, 'craft')
   assert.equal(decision.args.name, 'sword')
 })
+test('armed warrior patrols a sheltered base perimeter', () => {
+  const decision = autonomousProgressionDecision(bot([{ name: 'iron_sword', count: 1 }]), { profession: 'warrior', health: 20, food: 20, nearbyEntities: [], nearbyBlocks: [] }, [{ type: 'base', label: 'Base protetta' }])
+  assert.equal(decision.action, 'explore')
+  assert.equal(decision.args.patrol, true)
+})
 
 /* test('explorer profession chooses a visible discovery instead of random wandering', () => {
   const decision = autonomousProgressionDecision(bot([{ name: 'oak_log', count: 8 }, { name: 'crafting_table', count: 1 }, { name: 'wooden_pickaxe', count: 1 }, { name: 'wooden_axe', count: 1 }, { name: 'wooden_shovel', count: 1 }, { name: 'chest', count: 1 }, { name: 'furnace', count: 1 }, { name: 'torch', count: 16 }, { name: 'cobblestone', count: 16 }]), { profession: 'explorer', health: 20, food: 20, nearbyEntities: [], nearbyBlocks: [], visibleTargets: [{ name: 'village', x: 12, y: 64, z: -4, distance: 12 }] }, [])
